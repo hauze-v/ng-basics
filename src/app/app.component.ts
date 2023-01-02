3ca998cf2c03754877efa61e9c27821a1f1df271
Adam Hauze <adamhauze@Adams-MacBook-Pro.local>
2023-01-02 07:30:25 -0700

    recreated AppModule and AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<p>Hello world!</p>`
 })
 export class AppComponent {
-  title = 'ng-basics';
+
 }
