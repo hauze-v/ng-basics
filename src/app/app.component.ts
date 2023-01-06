d6c257b469028b7725f93cdcc47e83727dccc32e
Adam Hauze <adamhauze@Adams-MacBook-Pro.local>
2023-01-06 07:50:08 -0700

    re-wrote bootstrapped angular files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `<p>Hello world!</p>`
+  // template: `<p>Hello world!</p>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // styles: ['']
 })
 export class AppComponent {
 
